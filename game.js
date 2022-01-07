e834e907cc6e9a36d2c50a71f1c1da31ea3af148
Rumon <rumonuniv@gmail.com>
2022-01-07 13:23:00 +0900

    game-loop

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
 
 
+let prevTime = 0;
+// primary game loop
 function mainGame(time){
-
+    let deltaTime = 0;
+    if(prevTime !== null){
+        deltaTime = time - prevTime;
+    }
+    prevTime = time;
+    window.requestAnimationFrame(mainGame);
 }
+
+// initial call of the main game loop
+window.requestAnimationFrame(mainGame);
